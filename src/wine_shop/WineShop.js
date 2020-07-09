2d72936d162b3f4ad046f9f6661fe771b76685c8
Kate <katherinanik3@gmail.com>
2020-07-09 16:51:32 +0300

    startTest

---
+++ src/wine_shop/WineShop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './WineShop.css';
+
+function WineShop() {
+  return (
+    <div className="wrapper">
+        <div className='header'>
+        <div className ='headerPicture'/>
+    </div>
+    </div>
+  );
+}
+
+export default WineShop;
