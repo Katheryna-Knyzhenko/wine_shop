39100a42cb0fe39bb6dc193809ad3b0456f40236
Kate <katherinanik3@gmail.com>
2020-07-11 01:59:50 +0300

    first screen design done

--- src/wine_shop/WineShop.js
+++ src/wine_shop/WineShop.js
@@ -10,7 +10,10 @@ function WineShop() {
             <div className='grape'><img  className='grapeImg' src={require('../pictures/intro-grape11.png')} alt='grape'/></div>
             <div className='bottle'><img  className='bottleImg' src={require('../pictures/bottle (1).png')} alt='bottle'/></div>
             <div className='fullLogo'><img src={require('../pictures/full logo ver.jpg')} alt='logo'/></div>
-            <div className='grape4'><img className='grape4Img' src={require('../pictures/single-grape-main 4 (1).png')}/></div>
+            <div><img className='grape4Img' src={require('../pictures/single-grape-main 4 (1).png')}/></div>
+            <div><img className='grape1Img' src={require('../pictures/single-grape-main 1.png')}/></div>
+            <div><img className='grape2Img' src={require('../pictures/single-grape-main 2 (1).png')}/></div>
+            <div><img className='grape3Img' src={require('../pictures/single-grape-main 3.png')}/></div>
             <div className='exploreText'>Explore the world of wine</div>
             <div className='learnAbout'>Learn about the vast world of wine in a relaxed and fun atmosphere with other wine lovers and enthusiasts.
                 <br/>Wine trails are suitable for those who are already in love with the world of
