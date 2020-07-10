dff857a97dfacc7198c2afc215ec29dcc8dc7e1f
Kate <katherinanik3@gmail.com>
2020-07-11 01:14:21 +0300

    addPicturesZIndex

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WineShop from './wine_shop/WineShop';
+import Footer from './footer/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
     <WineShop/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
