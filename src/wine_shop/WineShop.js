7e4115ece5124c56b86902ac554d16009e899c54
Kate <katherinanik3@gmail.com>
2020-07-10 17:50:57 +0300

    addPicturesZIndex

--- src/wine_shop/WineShop.js
+++ src/wine_shop/WineShop.js
@@ -5,8 +5,11 @@ function WineShop() {
   return (
     <div className="wrapper">
         <div className='head'>
-        <div className ='headerPicture'/>
+        <div className ='headerPicture'>
         </div>
+            <div className='grape'><img  className='grapeImg' src={require('../pictures/intro-grape11.png')} alt='grape'/></div>
+            <div className='bottle'><img  className='bottleImg' src={require('../pictures/bottle (1).png')} alt='grape'/></div>
+    </div>
         <div className='middle'>
         <div className='middlePicture'/>
     </div>
