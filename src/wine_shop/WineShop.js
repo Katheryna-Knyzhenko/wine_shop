0e53b70c5c71798799b9bfe47f70089720281fb5
Kate <katherinanik3@gmail.com>
2020-07-11 00:59:27 +0300

    addPicturesZIndex

--- src/wine_shop/WineShop.js
+++ src/wine_shop/WineShop.js
@@ -10,7 +10,9 @@ function WineShop() {
             <div className='grape'><img  className='grapeImg' src={require('../pictures/intro-grape11.png')} alt='grape'/></div>
             <div className='bottle'><img  className='bottleImg' src={require('../pictures/bottle (1).png')} alt='grape'/></div>
             <div className='exploreText'>Explore the world of wine</div>
-            <div className='learnAbout'>Learn about the vast world of wine in a relaxed and fun atmosphere with other wine lovers and enthusiasts. Wine trails are suitable for those who are already in love with the world of wine and those who are just beginning to appreciate it.
+            <div className='learnAbout'>Learn about the vast world of wine in a relaxed and fun atmosphere with other wine lovers and enthusiasts.
+                <br/>Wine trails are suitable for those who are already in love with the world of
+                <br/>wine and those who are just beginning to appreciate it.
             </div>
     </div>
         <div className='middle'>
