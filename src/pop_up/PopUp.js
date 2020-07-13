1cb13058abf47344b290ecbd754d06936ab3d9e6
Kate <katherinanik3@gmail.com>
2020-07-13 13:47:17 +0300

    design

--- src/pop_up/PopUp.js
+++ src/pop_up/PopUp.js
@@ -10,7 +10,7 @@ function PopUp(props) {
                 <input className='telInput' value='Phone'></input>
                 <div className='userInfo'><span className='ageChoose'>Your age:</span>
                     <div className='agy'><label htmlFor='check1'>10 - 20<input id='check1' type="radio" name="n1" className='age20'/></label></div>
-                    <div className='agy'><label htmlFor='check2'>20 - 30<input id='check2' type="radio" name="n1"
+                    <div className='agy'><label htmlFor='check2'><span className="radio">20 - 30</span><input id='check2' type="radio" name="n1"
                                                                    className='age30'/></label>
                     </div>
                     <div className='agy'><label htmlFor='check3'>30 - 40<input id='check3' type="radio" name="n1"
@@ -20,7 +20,7 @@ function PopUp(props) {
                 </div>
                 <div className='buttons'>
                     <div className='downloadFoto'><span>Download your photo:</span>
-                        <button className='cloudB'><img src={require('../pictures/Vector (4).png')}/></button> </div>
+                        <button className='cloudB'></button> </div>
                     <button className='sendB'><span className='sendT'>Send</span></button>
                 </div>
             </div>
