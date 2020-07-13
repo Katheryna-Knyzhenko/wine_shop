048da924b3e7e66725b04a4271aa44f40d7f2942
Kate <katherinanik3@gmail.com>
2020-07-13 13:36:05 +0300

    popUoCloud

--- src/pop_up/PopUp.js
+++ src/pop_up/PopUp.js
@@ -9,14 +9,19 @@ function PopUp(props) {
                 <input className='nameInput' value='Name'></input>
                 <input className='telInput' value='Phone'></input>
                 <div className='userInfo'><span className='ageChoose'>Your age:</span>
-                    <div className='agy'><label htmlFor='n2'>10 - 20<input type="radio" name="n1" className='age20'/></label></div>
-                    <div className='agy'><label htmlFor='n2'>20 - 30<input type="radio" name="n1"
+                    <div className='agy'><label htmlFor='check1'>10 - 20<input id='check1' type="radio" name="n1" className='age20'/></label></div>
+                    <div className='agy'><label htmlFor='check2'>20 - 30<input id='check2' type="radio" name="n1"
                                                                    className='age30'/></label>
                     </div>
-                    <div className='agy'><label htmlFor='n3'>30 - 40<input type="radio" name="n1"
+                    <div className='agy'><label htmlFor='check3'>30 - 40<input id='check3' type="radio" name="n1"
                                                                                          className='age40'/></label>
                     </div>
 
+                </div>
+                <div className='buttons'>
+                    <div className='downloadFoto'><span>Download your photo:</span>
+                        <button className='cloudB'><img src={require('../pictures/Vector (4).png')}/></button> </div>
+                    <button className='sendB'><span className='sendT'>Send</span></button>
                 </div>
             </div>
         </div>
