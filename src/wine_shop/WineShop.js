556481a04eb99d0131dfcaafb94e2bb55007115a
Kate <katherinanik3@gmail.com>
2020-07-14 18:02:30 +0300

    add some animation

--- src/wine_shop/WineShop.js
+++ src/wine_shop/WineShop.js
@@ -4,10 +4,17 @@ import PopUp from "../pop_up/PopUp";
 import Menu from "../menu/Menu";
 import ScrollAnimation from 'react-animate-on-scroll';
 import animateIn from "animate.css/animate.min.css";
+import AOS from 'aos';
+import $ from 'jquery';
 
-
+AOS.init({
+    debounceDelay: 50,
+    once: true,
+    mirror: true,
+});
 
 class WineShop extends Component {
+
     constructor(props) {
         super(props);
         this.state = {isPopUpShown: false, isMenuShown: false, mainLogo: true};
@@ -15,7 +22,17 @@ class WineShop extends Component {
 
 
     render() {
-
+        AOS.init({
+            debounceDelay: 50,
+            once: true,
+            mirror: true,
+        });
+        $(document).ready(function () {
+        document.onmousemove = function () {
+            var introGrape1 = document.querySelector('#bot');
+            introGrape1.classList.add('bottleAnimate');
+        }
+    })
         const showMenu = () => {
             console.log('isMenuShown', this.state.isMenuShown);
             this.setState({isMenuShown: !this.state.isMenuShown})
@@ -27,11 +44,12 @@ class WineShop extends Component {
         const scroll = () => {
             console.log('1111', window.scrollY);
             window.scrollY === 0 ? this.setState({mainLogo: true}) : this.setState({mainLogo: false})
+
         };
 
         return (
 
-            <div className="wrapper" onWheel={scroll}>
+            <div className="wrapper" id='wrapper' onWheel={scroll}>
 
 
                 {this.state.mainLogo ?
@@ -47,7 +65,7 @@ class WineShop extends Component {
                     <div className='menu'>
                         <div className='item'>About us</div>
                         <div className='item'>History</div>
-                        <div className='item call' onClick={showPopUp}>Call me plz</div>
+                        <div className='item call'  onClick={showPopUp}>Call me plz</div>
                         <div className='item phone'><span>+35 987 654 32 10</span></div>
                     </div>
                     <div onClick={showMenu} className='burger'><img src={require('../pictures/Vector.png')}
@@ -55,18 +73,21 @@ class WineShop extends Component {
                         src={require('../pictures/Ellipse 1.png')} className='burgerRound'/></div>
                     <div className='headerPicture'>
                     </div>
-                    <div className='grape'><img className='grapeImg' src={require('../pictures/intro-grape11.png')}
+                    <div className='grape' ><img  className= 'grapeImg' src={require('../pictures/intro-grape11.png')}
                                                 alt='grape'/></div>
-                    <div className='bottle'><img src={require('../pictures/bottle (1).png')}
+
+
+                  <div  className='bottle aos-item' id = 'bot' ><img src={require('../pictures/bottle (1).png')}
                                                  alt='bottle'/></div>
 
-                    <div className='fullLogo'>
-                        <img id='fullLogoImg' src={require('../pictures/full logo ver.jpg')} alt='logo'/></div>
 
-                    <div><img className='grape4Img' src={require('../pictures/single-grape-main 4 (1).png')}/></div>
-                    <div><img className='grape1Img' src={require('../pictures/single-grape-main 1.png')}/></div>
-                    <div><img className='grape2Img' src={require('../pictures/single-grape-main 2 (1).png')}/></div>
-                    <div><img className='grape3Img' src={require('../pictures/single-grape-main 3.png')}/></div>
+                    <div className='fullLogo'>
+                        <img  data-aos='reveal-right' id='fullLogoImg' src={require('../pictures/full logo ver.jpg')} alt='logo'/></div>
+
+                    <div><img data-aos  = 'fade-in' data-aos='fade-out' id = 'grape4Img' className='grape4Img' src={require('../pictures/single-grape-main 4 (1).png')}/></div>
+                    <div><img className='grape1Img' id = 'grape1Img' src={require('../pictures/single-grape-main 1.png')}/></div>
+                    <div><img className='grape2Img' id = 'grape2Img' src={require('../pictures/single-grape-main 2 (1).png')}/></div>
+                    <div><img className='grape3Img' id = 'grape3Img' src={require('../pictures/single-grape-main 3.png')}/></div>
 
                     <div className='exploreText'>Explore the world of wine</div>
                     <div className='learnAbout'>Learn about the vast world of wine in a relaxed and fun atmosphere with
@@ -77,30 +98,30 @@ class WineShop extends Component {
                 </div>
                 <div className='middle'>
                     <div className='middlePicture'>
-                        <div><img className='introgrape13' src={require('../pictures/intro-grape1 3 (2).png')}/></div>
-                        <div><img className='introgrape12' src={require('../pictures/intro-grape1 2.png')}/></div>
-                        <div><img className='singleGrapeMain31'
+                        <div><img id = 'introgrape13' className='introgrape13' src={require('../pictures/intro-grape1 3 (2).png')}/></div>
+                        <div><img className='introgrape12' id = 'introgrape12' src={require('../pictures/intro-grape1 2.png')}/></div>
+                        <div><img id = 'singleGrapeMain31' className='singleGrapeMain31'
                                   src={require('../pictures/single-grape-main 3 (1).png')}/>
                         </div>
-                        <div><img className='singleGrapeMain1'
+                        <div><img className='singleGrapeMain1' id = 'singleGrapeMain1'
                                   src={require('../pictures/single-grape-main 1 (1).png')}/>
                         </div>
-                        <div><img className='singleGrapeMain3' src={require('../pictures/single-grape-main 3.png')}/>
+                        <div><img className='singleGrapeMain3' id = 'singleGrapeMain3' src={require('../pictures/single-grape-main 3.png')}/>
                         </div>
                         <div><img className='ellipse' src={require('../pictures/Ellipse 2.png')}/>
                             <div><img className='n1941' src={require('../pictures/1901 (1).png')}/></div>
                         </div>
-                        <div className='midText'>Give the wine lover in your life a gift they will love. Wine trails are
+                        <div data-aos = 'fade-down' data-aos-duration = '400' className='midText'>Give the wine lover in your life a gift they will love. Wine trails are
                             a fun way of exploring the great food and wine venues we have in the city while making great
                             memories
                         </div>
-                        <div><img className='leaveBig'
+                        <div><img id = 'leaveBig' className='leaveBig'
                                   src={require('../pictures/kisspng-leaf-clip-art-grape-vine-leaf-5a754b8e03a590 3.png')}/>
                         </div>
-                        <div><img className='leaveSmaller1'
+                        <div><img className='leaveSmaller1' id = 'leaveSmaller1'
                                   src={require('../pictures/kisspng-leaf-clip-art-grape-vine-leaf-5a754b8e03a590 2.png')}/>
                         </div>
-                        <div><img className='leaveSmaller2'
+                        <div><img className='leaveSmaller2' id = 'leaveSmaller2'
                                   src={require('../pictures/kisspng-leaf-clip-art-grape-vine-leaf-5a754b8e03a590 1.png')}/>
                         </div>
 
@@ -113,13 +134,13 @@ class WineShop extends Component {
                         weather conditions that allow us to grow both exotic international grapes as well as local
                         traditional grape varieties.
                     </div></ScrollAnimation>
-                    <div><img className='glassLeft'
+                    <div><img className='glassLeft' id = 'glassLeft'
                               src={require('../pictures/glass 1.png')}/>
                     </div>
                     <button  className='recButton'><span className='textLearnMore'>
                   Learn more</span>
                     </button>
-                    <div><img className='glassRight'
+                    <div><img className='glassRight' id = 'glassRight'
                               src={require('../pictures/glass 2.png')}/>
                     </div>
                     <div className='bottomPicture'/>
